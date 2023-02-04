0fe19c877a2c43885cabab9e03edd6668c102ad5
“eastjin” <afaset4444@naver.com>
2023-02-04 11:23:45 +0900

    first commit

---
+++ src/main/java/org/example/busmake/Bus.java
@@ -0,0 +1,2 @@
+package org.example.busmake;public class Bus {
+}
---
+++ src/main/java/org/example/busmake/Vehicle.java
@@ -0,0 +1,19 @@
+package org.example.busmake;
+//        1. 최대 승객수
+//        2. 현재 승객수
+//        3. 요금
+//        4. 버스 번호
+//        5. 주유량
+//        6. 현재 속도
+//        7. 상태
+//        1.  운행, 차고지 행
+public class Bus {
+    int max_Passenger;
+    int current_Passenger;
+    int pay;
+    int bus_Num;
+    int amount_Gas;
+    int current_Speed;
+    boolean status_Bus = true;
+
+}
